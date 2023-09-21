8f9f21fc68abf4b5c7c8985088000beed6e0b83f
Muhzin Firoz ALIKKAL <firozalikkal@gmail.com>
2023-09-21 09:30:00 +0530

    Enhanced Airflow DAG with dbt execution and data ingestion tasks.

--- airflow/dags/data_pipeline.py
+++ airflow/dags/data_pipeline.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+import subprocess
 
 def run_dbt():
-    # dbt run logic
-    pass
+    subprocess.run(['dbt', 'run'], check=True)
 
 default_args = {
     'owner': 'airflow',
@@ -16,7 +16,14 @@ with DAG(
     default_args=default_args,
     schedule_interval='@daily',
 ) as dag:
-    run_dbt = PythonOperator(
-        task_id='run_dbt',
+    ingest_data = PythonOperator(
+        task_id='ingest_data',
+        python_callable=lambda: print("Ingesting data..."),
+    )
+
+    transform_data = PythonOperator(
+        task_id='transform_data',
         python_callable=run_dbt,
     )
+
+    ingest_data >> transform_data
