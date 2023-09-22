2faba0191b03c096205f08b9957146bedf67ff68
Muhzin Firoz ALIKKAL <firozalikkal@gmail.com>
2023-09-22 11:15:17 +0530

    dbaaeeab50c0cf7f2493e1cccbe083d497da1289

--- airflow/dags/data_pipeline.py
+++ airflow/dags/data_pipeline.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-import subprocess
 
 def run_dbt():
-    subprocess.run(['dbt', 'run'], check=True)
+    # dbt run logic
+    pass
 
 default_args = {
     'owner': 'airflow',
@@ -16,14 +16,7 @@ with DAG(
     default_args=default_args,
     schedule_interval='@daily',
 ) as dag:
-    ingest_data = PythonOperator(
-        task_id='ingest_data',
-        python_callable=lambda: print("Ingesting data..."),
-    )
-
-    transform_data = PythonOperator(
-        task_id='transform_data',
+    run_dbt = PythonOperator(
+        task_id='run_dbt',
         python_callable=run_dbt,
     )
-
-    ingest_data >> transform_data
